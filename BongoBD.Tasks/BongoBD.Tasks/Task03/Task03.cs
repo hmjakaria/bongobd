86f3454fbd62f3a2e02cc892929c71e387cdad30
hmjakaria <HossainMJakaria@gmail.com>
2019-05-25 00:43:28 +0600

    Revert "Update Task03.cs"
    This reverts commit 57a5a93e494d9895affdbd22966eb848389a9fe9.

--- BongoBD.Tasks/BongoBD.Tasks/Task03/Task03.cs
+++ BongoBD.Tasks/BongoBD.Tasks/Task03/Task03.cs
@@ -36,8 +36,6 @@ namespace BongoBD.Tasks.Task03
             return ancestorsOfNode1.Intersect(ancestorsOfNode2)?.FirstOrDefault();
         }
 
-
-        //Memory
         //Solution for Simple Least Common Ancestor with recursion
         public static Node Lca2(Node node1, Node node2)
         {
